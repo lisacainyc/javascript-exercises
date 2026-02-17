f94611207412b7476ba703ab7b5712758104c634
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-02-17 12:54:20 -0800

    Complete first two Javascript exercises

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arrayStr = str.split("");
+    let reverseStr = "";
+    for (let i = 1; i <= arrayStr.length; i++){
+        reverseStr = reverseStr + arrayStr[(arrayStr.length) - i];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
