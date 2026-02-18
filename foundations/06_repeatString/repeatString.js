1c1ff562aed3fce4b478699abf4b63311d95bae8
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-02-18 14:14:15 -0800

    Complete two more Javascript exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+if (num < 0){
+    return "ERROR";
+}
+    let newStr = "";
+    for (let i = num; i > 0; i--){
+    newStr = newStr + str;
+        };
+    return newStr;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(...arr) {
+    let sortedArray = arr.sort((a, b) => a - b);
+    for (let n = 0; n < sortedArray.length; n++){
+        if ((typeof sortedArray[n] !== "number") || (!Number.isInteger(sortedArray[n]))|| (sortedArray[n] < 0)){
+            return("ERROR");
+        }
+    }
+    let sum = 0;
+    for (let i = sortedArray[1]; i >= sortedArray[0]; i--){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
