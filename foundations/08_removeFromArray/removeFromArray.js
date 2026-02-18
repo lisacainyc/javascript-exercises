933df7306b2c103bed7bfb494611c7e6e2a1e4e1
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-02-18 13:18:42 -0800

    Complete two array exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const argsArray = Array.from(args);
+    let newArray = arr.filter(item => !argsArray.includes(item));
+    return(newArray);
 };
 
 // Do not edit below this line
