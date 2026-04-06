4a84acf4ef086fd2f14ce7bb16085799029e812b
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-04-06 10:26:18 -0700

    complete Javascript foundations exercises

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let celcius = (far - 32) / (9/5);
+  celcius = (Math.round(celcius * 10))/10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+    let farenheit = cel * 9/5 + 32;
+    farenheit = (Math.round(farenheit*10))/10;
+    return farenheit;
 };
 
 // Do not edit below this line
