23057373ff5eac0f652576550f5e4ea4c250ddba
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-04-07 08:32:10 -0700

    Complete palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let charArray = str.replace(/[^a-z0-9]/gi, '').toLowerCase().split('');
+    let reverseArray = str.replace(/[^a-z0-9]/gi, '').toLowerCase().split('').reverse();
+    for (let i = 0; i < charArray.length; i++){
+        if (charArray[i] !== reverseArray[i]){
+            return false;
+        };
+    };
+    return true;
 };
 
 // Do not edit below this line
