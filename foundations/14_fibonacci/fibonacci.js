5a306edfc80e9e5fb635f8b4e825a457529bf832
lisacainyc <237272486+lisacainyc@users.noreply.github.com>
2026-04-07 11:29:48 -0700

    Complete Fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = Number(n);
+    if (n < 0) {
+        return "OOPS";
+    }else if (n == 0){
+        return 0;
+    }else{
+    let calc = [1,1];
+    for (let i = 0; i < n-2; i++){
+        let nextNum = calc[i] + calc[i+1];
+        calc.push(nextNum);
+    }; 
+    return calc.pop();
+    };
 };
 
 // Do not edit below this line
